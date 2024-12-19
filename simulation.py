1eac4cff139833a9e0d49a13711b2db5787c31a8
sejun-Park3312 <dlrhddlfvkf21207@pusan.ac.kr>
2024-12-19 16:13:34 +0900

    '24.12.19'

--- simulation.py
+++ simulation.py
@@ -1,5 +1,10 @@
 import pybullet as p
+import pybullet_data
+
 pysicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(gravX = 0, gravY = 0, gravZ = -9.8)
+planeId = p.loadURDF('plane.urdf')
 p.loadSDF("box.sdf")
 import time
 for i in range(1000):
