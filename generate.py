3304243705da0518e96d8c8c810dfa2eadb60af7
sejun-Park3312 <dlrhddlfvkf21207@pusan.ac.kr>
2024-12-19 16:46:37 +0900

    '24.12.19'

--- generate.py
+++ generate.py
@@ -1,4 +1,14 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF('box.sdf')
-pyrosim.Send_Cube(name='Box', pos=[0,0,0.5] , size=[1,1,1])
-pyrosim.End()
+
+def create_robot():
+    pyrosim.Start_URDF('body.urdf')
+
+    pyrosim.Send_Cube(name='Torso', pos=[0,0,0.5] , size=[1,1,1])
+
+    pyrosim.Send_Joint(name = 'Torso_R_Leg', parent = 'Torso', child = 'R_Leg', type = 'fixed', position = [0.5, 0, 1.5])
+    pyrosim.Send_Cube(name = 'R_Leg', pos = [0.5,0,0], size = [1,1,1])
+
+    pyrosim.Send_Joint(name = 'Torso_L_Leg', parent = 'Torso', child = 'L_Leg', type = 'fixed', position = [-0.5, 0, 1.5])
+    pyrosim.Send_Cube(name = 'L_Leg', pos = [-0.5,0,0], size = [1,1,1])
+
+    pyrosim.End()
--- simulation.py
+++ simulation.py
@@ -2,10 +2,11 @@ import pybullet as p
 import pybullet_data
 
 pysicsClient = p.connect(p.GUI)
+p.setRealTimeSimulation(1)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(gravX = 0, gravY = 0, gravZ = -9.8)
 planeId = p.loadURDF('plane.urdf')
-p.loadSDF("box.sdf")
+p.loadURDF("body.urdf")
 import time
 for i in range(1000):
     p.stepSimulation()
